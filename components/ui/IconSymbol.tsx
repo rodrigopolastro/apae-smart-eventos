e2b68b47e29c3260cbef91b1b7cd40d90b7f99ed
Rodrigo Polastro <rodrigo.polastro03@gmail.com>
2025-06-12 21:16:56 -0300

    feat: add new icons for the 'associate' bottom nav bar tabs

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -18,6 +18,7 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'admin.icon': 'admin-panel-settings',
+  'tickets.icon': 'auto-awesome-motion',
 } as IconMapping;
 
 /**
